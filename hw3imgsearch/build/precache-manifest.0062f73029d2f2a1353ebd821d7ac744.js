aec502204ae4a5557bc23ef8690b5427e270843f
bor9sik <asdf514asdf514@gmail.com>
2019-09-10 15:59:26 +0300

    hw3

---
+++ hw3imgsearch/build/precache-manifest.0062f73029d2f2a1353ebd821d7ac744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb89efd9e0db052f06a7a4964780012e",
+    "url": "/reactHW/hw3imgsearch/build/index.html"
+  },
+  {
+    "revision": "0c0abb5e67d052d817e7",
+    "url": "/reactHW/hw3imgsearch/build/static/css/main.961b844e.chunk.css"
+  },
+  {
+    "revision": "4aa0bef75c6b101a2798",
+    "url": "/reactHW/hw3imgsearch/build/static/js/2.7d8e08e3.chunk.js"
+  },
+  {
+    "revision": "0c0abb5e67d052d817e7",
+    "url": "/reactHW/hw3imgsearch/build/static/js/main.a05257f6.chunk.js"
+  },
+  {
+    "revision": "f344a90515266714ac1c",
+    "url": "/reactHW/hw3imgsearch/build/static/js/runtime~main.40dad8f0.js"
+  }
+]);
---
+++ hw3imgsearch/build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactHW/hw3imgsearch/build/precache-manifest.0062f73029d2f2a1353ebd821d7ac744.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactHW/hw3imgsearch/build/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
