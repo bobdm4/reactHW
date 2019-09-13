db4b32019f9314180a9652c83507ed7bd149df16
bor9sik <asdf514asdf514@gmail.com>
2019-09-13 11:25:50 +0300

    Updates

---
+++ precache-manifest.3fe0db916b70b813d57431f2cf7ce6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabac2e6a62ed2eedc95",
+    "url": "/reactHW/hw4pets/static/js/runtime~main.c94da469.js"
+  },
+  {
+    "revision": "00920e56979cf2792777",
+    "url": "/reactHW/hw4pets/static/js/main.d3d17a1a.chunk.js"
+  },
+  {
+    "revision": "a8fe43774d6a8cf50e35",
+    "url": "/reactHW/hw4pets/static/js/2.653250ec.chunk.js"
+  },
+  {
+    "revision": "00920e56979cf2792777",
+    "url": "/reactHW/hw4pets/static/css/main.3fad6e47.chunk.css"
+  },
+  {
+    "revision": "96a695838703bc27783dd1c1f023c036",
+    "url": "/reactHW/hw4pets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactHW/precache-manifest.c68fac68321a8504a064ff17837486c0.js"
+  "/reactHW/hw4pets/precache-manifest.3fe0db916b70b813d57431f2cf7ce6f9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactHW/index.html", {
+workbox.routing.registerNavigationRoute("/reactHW/hw4pets/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
