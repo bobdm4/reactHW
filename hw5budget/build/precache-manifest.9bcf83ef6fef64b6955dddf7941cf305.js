19733bdcf11c81831e090b6a4e7b1b480642f776
bor9sik <asdf514asdf514@gmail.com>
2019-09-16 23:46:26 +0300

    hw5

---
+++ hw5budget/build/precache-manifest.9bcf83ef6fef64b6955dddf7941cf305.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02bf60fbd4a73d4188d8",
+    "url": "/reactHW/hw5budget/build/static/js/runtime~main.6a216ad3.js"
+  },
+  {
+    "revision": "dc10414443e6aad96d82",
+    "url": "/reactHW/hw5budget/build/static/js/main.e2a954fd.chunk.js"
+  },
+  {
+    "revision": "e369ef0c3ae0f10d6056",
+    "url": "/reactHW/hw5budget/build/static/js/2.ef9cc998.chunk.js"
+  },
+  {
+    "revision": "dc10414443e6aad96d82",
+    "url": "/reactHW/hw5budget/build/static/css/main.fd4407f5.chunk.css"
+  },
+  {
+    "revision": "fcdac18d16a8946be814e16c7a2dfc4d",
+    "url": "/reactHW/hw5budget/build/index.html"
+  }
+];
